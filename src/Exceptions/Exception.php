59ad9ff946e73a0d7b0791d959e71ba374f202f0
vagrant <vagrant@homestead>
2020-06-29 01:29:07 +0000

    init

---
+++ src/Exceptions/Exception.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Jiulin\Weather\Exceptions;
+
+class Exception extends \Exception
+{
+
+}
---
+++ src/Exceptions/InvalidArgumentException.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace Jiulin\Weather\Exceptions;
+
+class InvalidArgumentException extends Exception
+{
+
+}
